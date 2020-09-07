a0d7e60767efb0049bf08f83a1f5e72dc75c35d4
Javohir-cryptic <javohirerkinovich@gmail.com>
2020-09-07 22:34:51 +0300

    Initial Commit

---
+++ Do-while.cpp
@@ -0,0 +1,23 @@
+﻿/*Ввести натуральное число и найти сумму его цифр. Организовать ввод числа так, что-
+бы нельзя было ввести отрицательное число или ноль.*/
+
+#include <stdio.h>
+#include <iostream>
+#include <locale.h>
+#pragma warning(disable : 4996)
+
+int main()
+{
+    int N, sum = 0;
+    setlocale(LC_ALL, "Rus");
+    do {
+        printf("\nВведите натуралное число: ");
+        scanf("%d", &N);
+    } while (N <= 0);
+    while (N > 0) {
+        sum += N % 10;
+        N /= 10;
+    }
+    printf("Сумма цифр этого числа равна %d\n", sum);
+    getchar();
+}
